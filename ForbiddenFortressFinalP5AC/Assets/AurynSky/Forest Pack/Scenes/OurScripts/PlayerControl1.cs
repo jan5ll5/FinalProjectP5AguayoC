012a108c5b9b848316b8113891701da718b006f9
janelle <731364@student.dusd.net>
2024-05-22 13:32:42 -0700

    added scripts and deleted scripts
    i made a player control script and deleted scripts we didn't need

---
+++ ForbiddenFortressFinalP5AC/Assets/AurynSky/Forest Pack/Scenes/OurScripts/PlayerControl1.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerControl1 : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+    }
+}
