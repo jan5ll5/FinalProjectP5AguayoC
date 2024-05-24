fa20391bb33c8f78d2b8cfac3c74d2ff0956217a
janelle <731364@student.dusd.net>
2024-05-24 13:49:15 -0700

    added text and made scripts
    i made 2 script, follow player and game manager. i added the score text, game over text, start and restart buttons, and title text

---
+++ ForbiddenFortressFinalP5AC/Assets/AllStarCharacterLibrary/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button startButton;
+    public Button restartButton;
+    public bool isGameActive;
+
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameActive = true;
+        score = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
